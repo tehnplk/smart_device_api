11cdfdbcbbce514668a05f0737dc0da4620bd19c
tehnplk <tehnplk@gmail.com>
2020-08-24 14:57:14 +0700

    tehnn

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var personRouter = require('./routes/person');
 var bmiRouter = require('./routes/bmi');
+var bpRouter = require('./routes/bp');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/person',personRouter);
 app.use('/bmi' ,bmiRouter);
+app.use('/bp' ,bpRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/bp.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var router = express.Router();
+
+var knex = require('./con_db');
+
+router.get('/', function(req, res, next) {
+  res.render('bmi', { title: 'BP' });
+});
+
+router.post('/add_log',async function(req,res,next){
+
+  let vn = req.body.vn;
+  let cid = req.body.cid;
+  let bps = req.body.bps;
+  let bpd = req.body.bpd;
+  let pulse = req.body.pulse;
+  let hn = req.body.hn;
+  let fullname = req.body.fullname;
+  let note1 = req.body.note1;
+  let note2 = req.body.note2;
+  let note3 = req.body.note3;
+
+  d_update = new Date();
+  console.log(d_update)
+
+  var id = await knex('smart_gate_bp')
+  .insert({id:null,vn:vn,cid:cid,bps:bps,bpd:bpd,pulse:pulse,hn:hn,fullname:fullname,d_update:d_update,note1:note1,note2:note2,note3:note3})
+  res.json({
+    'id':id[0]
+  })
+});
+
+router.post('/update_opdscreen',async function(req,res,next){
+
+  let vn = req.body.vn;
+  let bps = req.body.bps;
+  let bpd = req.body.bpd;
+  let pulse = req.body.pulse;
+
+  let effect = await knex('opdscreen')
+  .where('vn', '=', vn)
+  .update({
+      bps:bps,
+      bpd:bpd,
+      pulse:pulse
+  })
+
+  res.json({
+    'effect':effect
+  })
+});
+
+router.post('/add_opdscreen_bp',async function(req,res,next){
+
+  let vn = req.body.vn;  
+  let bps = req.body.bps;
+  let bpd = req.body.bpd;
+  let pulse = req.body.pulse;
+  let depcode = req.body.depcode;
+  let staff = req.body.staff;
+  let sql = ` replace into opdscreen_bp 
+  set opdscreen_bp_id = get_serialnumber('opdscreen_bp_id') 
+  ,vn =? ,bps=? ,bpd=? ,pulse=? ,depcode=? ,staff=? 
+  ,screen_date = CURRENT_DATE,screen_time = CURRENT_TIME ,rr=0,o2sat=0,temperature=0 `;
+
+  let data = await knex.raw(sql,[vn,bps,bpd,pulse,depcode,staff]);
+  console.log(data[0]);
+  res.json({
+    'opdscreen_bp':'added'
+  });
+
+});
+
+
+
+module.exports = router;
