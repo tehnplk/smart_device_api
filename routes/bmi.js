650cad95f33573f87d421e0612df7a5009b6fbd5
tehnplk <tehnplk@gmail.com>
2020-08-24 14:27:52 +0700

    tehnn

--- routes/bmi.js
+++ routes/bmi.js
@@ -16,11 +16,15 @@ router.post('/add_log',async function(req,res,next){
   let bmi = req.body.bmi;
   let hn = req.body.hn;
   let fullname = req.body.fullname;
+  let note1 = req.body.note1;
+  let note2 = req.body.note2;
+  let note3 = req.body.note3;
+
   d_update = new Date();
   console.log(d_update)
 
   var id = await knex('smart_gate_bmi')
-  .insert({id:null,vn:vn,cid:cid,bw:bw,bh:bh,bmi:bmi,hn:hn,fullname:fullname,d_update:d_update})
+  .insert({id:null,vn:vn,cid:cid,bw:bw,bh:bh,bmi:bmi,hn:hn,fullname:fullname,d_update:d_update,note1:note1,note2:note2,note3:note3})
   res.json({
     'id':id[0]
   })
